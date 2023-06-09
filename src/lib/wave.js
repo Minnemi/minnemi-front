726d53c1ffc2c4ffccede9b3d6c723b70c06c883
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-08 22:29:02 -0300

    chore: changes canvas width

--- src/lib/wave.js
+++ src/lib/wave.js
@@ -4,13 +4,13 @@ function Point(index, x, y, speed) {
   this.y = y;
   this.fixedY = y;
   this.speed = speed;
-  this.cur = index;
+  this.currentPoint = index;
   this.max = Math.random() * 100 + 15;
 }
 
 Point.prototype.update = function() {
-  this.cur += this.speed;
-  this.y = this.fixedY + (Math.sin(this.cur) * this.max);
+  this.currentPoint += this.speed;
+  this.y = this.fixedY + (Math.sin(this.currentPoint) * this.max);
 };
 
 function Wave(index, {totalPoints, speed, gradColor}) {
@@ -26,7 +26,7 @@ Wave.prototype.resize = function(stageWidth, stageHeight) {
   this.stageHeight = stageHeight;
   this.centerX = stageWidth / 2;
   this.centerY = stageHeight / 2;
-  this.pointGap = this.stageWidth / (this.totalPoints -1);
+  this.pointGap = this.stageWidth / (this.totalPoints - 1);
   this.init(this.speed);
 };
 
@@ -43,13 +43,13 @@ Wave.prototype.draw = function(context) {
   const gradient = context.createLinearGradient(0, 0, this.stageWidth, 0);
 
   for (let i = 0; i < this.color.length; i++) {
-    gradient.addColorStop( (1 / this.color.length) * i, this.color[i]);
+    gradient.addColorStop((1 / this.color.length) * i, this.color[i]);
   }
 
   context.beginPath();
   context.fillStyle = gradient;
 
-  if (this.points.length < 1) return;
+  if (this.points.length) return;
 
   let prevX = this.points[0].x;
   let prevY = this.points[0].y;
@@ -110,7 +110,6 @@ WaveGroup.prototype.draw = function(context) {
 /**
  *
  * @param {Boolean} enableAnim
- * @param {HTMLElement} anchor
  */
 function Main(enableAnim = false) {
   this.enableAnim = enableAnim;
@@ -131,9 +130,9 @@ function Main(enableAnim = false) {
 Main.prototype.resize = function() {
   this.stageWidth = this.anchor.clientWidth;
   this.stageHeight = 300;
-  this.canvas.width = this.stageWidth;
-  this.canvas.height = this.stageHeight;
-  this.context.scale(1, 1);
+  this.canvas.width = this.stageWidth * 2;
+  this.canvas.height = this.stageHeight * 2;
+  this.context.scale(2, 2);
   this.wavegroup.resize(this.stageWidth, this.stageHeight);
 
   if (!this.enableAnim) {
