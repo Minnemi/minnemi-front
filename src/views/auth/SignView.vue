39cc9fb8f8648ad77e41de20d87d3e89540050fc
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-14 22:07:00 -0300

    feat: creates sign in view style

--- src/views/auth/SignView.vue
+++ src/views/auth/SignView.vue
@@ -1,9 +1,90 @@
 <template>
-  <div>
-    <h1>Entrar</h1>
-  </div>
+  <WrapperForm
+    title="Acessa conta"
+    description="comece hoje a escrever seu futuro"
+  >
+    <section class="fields">
+      <TextInput
+        label="Nome de usuário"
+        :attributes="attributes.username"
+      />
+
+      <TextInput
+        label="Senha"
+        :attributes="attributes.password"
+        :icon="mdiEye"
+        :enableButton="true"
+      />
+    </section>
+
+    <nav class="actions">
+      <ButtonComponent type="submit" styles="fill font-normal" full-width>
+        Acessar conta
+      </ButtonComponent>
+
+      <ButtonComponent styles="fill login-social font-normal" full-width>
+        <template v-slot:start>
+          <GoogleBrand />
+        </template>
+
+        Entrar com o Google
+      </ButtonComponent>
+    </nav>
+
+    <hr />
+
+    <nav class="extra-options">
+      <RouterLink to="/auth/recover" class="link">
+        Esqueceu sua senha? <span>Recuperar conta</span>
+      </RouterLink>
+
+      <RouterLink to="/auth/register" class="link">
+        Ainda não tem uma conta? <span>Crie uma conta</span>
+      </RouterLink>
+    </nav>
+  </WrapperForm>
 </template>
 
-<script setup></script>
+<script setup>
+import WrapperForm from '@@forms/WrapperForm.vue';
+import TextInput from '@@forms/TextInput.vue';
+import ButtonComponent from '@components/ButtonComponent.vue';
+import GoogleBrand from '@@utils/GoogleBrandIcon.vue';
+import { mdiEye } from '@mdi/js';
+import attributes from '@@shared/commonInputFieldsAttributes';
+</script>
 
-<style lang="scss" scoped></style>
+<style scoped lang="scss">
+.actions,
+.fields,
+.extra-options {
+  display: flex;
+  flex-direction: column;
+}
+
+.actions {
+  gap: .75rem;
+}
+
+.fields {
+  gap: .5rem;
+}
+
+.extra-options {
+  font-size: small;
+  text-align: center;
+}
+
+.link {
+  color: var(--light-100);
+
+  span {
+    font-weight: 600;
+    color: var(--red-200);
+  }
+
+  &:hover span {
+    text-decoration: underline;
+  }
+}
+</style>
