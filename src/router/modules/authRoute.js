d3ae9633c244bd80339ee1678d13d1febe86e4f7
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-15 04:29:08 -0300

    chore: includes recover page route

--- src/router/modules/authRoute.js
+++ src/router/modules/authRoute.js
@@ -8,6 +8,12 @@ const verifyIsAuthenticated = (to, from, next) => {
   appStore.isLogged ? next({ path: '/app' }) : next();
 };
 
+const recoverRoute = createRoute(
+  '/auth/recover',
+  import('@views/auth/RecoverView.vue'),
+  'auth',
+);
+
 const registerRoute = createRoute(
   '/auth/register',
   import('@views/auth/RegisterView.vue'),
@@ -25,7 +31,7 @@ const authRoute = createRoute(
   import('@views/auth/AuthView.vue'),
   'app',
   {
-    children: [registerRoute, signRoute],
+    children: [recoverRoute, registerRoute, signRoute],
     redirect: '/auth/sign-in',
     beforeEnter: verifyIsAuthenticated,
   },
