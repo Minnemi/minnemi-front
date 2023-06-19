d5e638b54921a452359a3758c5066b9bfa0bebe8
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-18 21:21:31 -0300

    chore: includes styles

--- src/components/utils/ChangeThemeComponent.vue
+++ src/components/utils/ChangeThemeComponent.vue
@@ -1,14 +1,15 @@
 <template>
-  <button @click="changeTheme">
+  <ButtonComponent styles="simple outline" @click="changeTheme">
     <IconComponent :path="mdiPalette" :size="36"></IconComponent>
-  </button>
+  </ButtonComponent>
 </template>
 
 <script setup>
 import { mdiPalette } from '@mdi/js';
-import IconComponent from '../utils/IconComponent.vue';
+import ButtonComponent from './ButtonComponent.vue';
+import IconComponent from './IconComponent.vue';
 import { onMounted, reactive } from 'vue';
-import { useAppStore } from '../../stores/appStore';
+import { useAppStore } from '@@stores/appStore';
 
 const appStore = useAppStore();
 
@@ -34,8 +35,4 @@ onMounted(
 
 <style module></style>
 
-<style scoped>
-button:hover {
-  color: var(--red-200);
-}
-</style>
+<style scoped></style>
