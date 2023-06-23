9507f30561a3796e47aa2fbbabca6c122a262e56
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-06-23 05:13:01 -0300

    chore: adds disabled style

--- src/components/utils/ButtonComponent.vue
+++ src/components/utils/ButtonComponent.vue
@@ -61,6 +61,11 @@ onMounted(() => {
   overflow: hidden;
 
   transition: background-color .2s ease, border-color .2s ease;
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: .5;
+  }
 }
 
 .simple {
