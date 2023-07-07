523fe119a0c07b63f397959abdb7b3daaa921de2
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-07-07 01:16:01 -0300

    chore: remove input pre validation

--- src/components/forms/TextInput.vue
+++ src/components/forms/TextInput.vue
@@ -5,7 +5,12 @@
     </div>
 
     <div ref="inputBoxRef" class="input">
-      <input ref="inputRef" class="input-field" @input="handleInput" />
+      <input
+        ref="inputRef"
+        class="input-field"
+        :data-compare-name="compareField?.name"
+        :data-compare-label="compareField?.label"
+      />
 
       <button
         type="button"
@@ -25,7 +30,6 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import SvgIcon from '@jamescoyle/vue-icon';
-import { describeError } from '@@shared/inputErrorsMessages.js';
 
 const inputBoxRef = ref(null);
 const inputRef = ref(null);
@@ -47,88 +51,11 @@ const props = defineProps({
     default: false,
   },
   focus: Boolean,
-  resetFieldOnInput: String,
   compareField: Object,
 });
 
 function handleClick() {
   inputRef.value.type = inputRef.value.type === 'password' ? 'text' : 'password';
-}
-
-function resetField() {
-  if (props.resetFieldOnInput) {
-    const fieldToReset = document.getElementsByName(props.resetFieldOnInput)[0];
-
-    if (fieldToReset && fieldToReset.value !== fieldToReset.defaultValue) {
-      fieldToReset.value = fieldToReset.defaultValue;
-      fieldToReset.dispatchEvent(new Event('input'));
-    }
-  }
-}
-
-function execCheckValidity(msg = '') {
-  inputRef.value.setCustomValidity(msg);
-
-  const buttonSubmitForm = document.querySelector('[data-submit-form]') || null;
-
-  if (!inputRef.value.checkValidity()) {
-    inputBoxRef.value.classList.add('error');
-    errorMessage.value = inputRef.value.validationMessage;
-    buttonSubmitForm && buttonSubmitForm.setAttribute('disabled', true);
-    return;
-  }
-
-  inputBoxRef.value.classList.remove('error');
-  errorMessage.value = '';
-  buttonSubmitForm && buttonSubmitForm.removeAttribute('disabled');
-}
-
-function validation() {
-  if (props.compareField) {
-    const compareFieldValue = document.getElementsByName(
-      props.compareField.name,
-    )[0].value;
-
-    let msg;
-
-    if (compareFieldValue !== inputRef.value.value) {
-      msg = describeError.mustBeEquals(props.compareField.label);
-    }
-
-    return execCheckValidity(msg);
-  }
-
-  const validity = inputRef.value.validity;
-
-  if (
-    validity.patternMismatch ||
-    validity.typeMismatch
-  ) {
-    const msg = describeError.pattern(inputRef.value.dataset.acceptedChars);
-    return execCheckValidity(msg);
-  }
-
-  if (validity.valueMissing) {
-    const msg = describeError.required();
-    return execCheckValidity(msg);
-  }
-
-  if (validity.tooShort) {
-    const msg = describeError.minLength(inputRef.value.minLength);
-    return execCheckValidity(msg);
-  }
-
-  if (validity.tooLong) {
-    const msg = describeError.maxLength(inputRef.value.maxLength);
-    return execCheckValidity(msg);
-  }
-
-  execCheckValidity('');
-}
-
-function handleInput() {
-  resetField();
-  validation();
 }
 
 function insertAttributesIntoInputElement(attributes) {
